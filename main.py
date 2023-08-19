46fd7a6e2d3356a9da98b4a9928b5e3d3a99fa56
mjiid <Habush1610@gmail.com>
2023-08-19 23:52:02 +0100

    added dataframe_to_json

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+from src.schemas.matchedResume import ResumeMatchedModel
+from src.schemas.extractedJob import JobModel
+from src.db_utils.db_connector import db_connect
+from src.feature_extraction.jobInfoExtraction import JobInfoExtraction
+from src.feature_extraction.Matcher import Matcher
+import json
+import pandas as pd
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
 
 
-def main():
-    pass
+def dataframe_to_json(dataframe):
+    # Transforms dataframe into a json file format
+    
+    temp = dataframe.to_json(orinet="records")
+    parsed = json.loads(temp)
+    json_data = json.dumps(parsed, indent=4)
 
+    return json_data
 
 
 
 if __name__ == "__main__":
-    main()
+    pass
 
 
