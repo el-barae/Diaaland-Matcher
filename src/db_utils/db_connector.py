b3b3e43115f95c3b46de12b675e14ec8bd8ed199
mjiid <Habush1610@gmail.com>
2023-08-24 16:05:40 +0100

    add connect to database

--- src/db_utils/db_connector.py
+++ src/db_utils/db_connector.py
@@ -1,11 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine import URL
 
 
-def db_connect(database_url):
+def db_connect(drivername, username, host, database):
 
+    database_url = URL.create(
+        drivername=drivername,
+        username=username,
+        host=host,
+        database=database
+    )
     # Create a PostgreSQL database engine and session
     engine = create_engine(database_url)
+    connection = engine.connect()
+
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-    return SessionLocal
+    return SessionLocal()
