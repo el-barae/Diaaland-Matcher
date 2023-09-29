07b7cf093eb2c7c4f5c0f98f71a105635aeafbdf
mjiid <habush1610@gmail.com>
2023-09-29 13:48:35 +0100

    add helper functions

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from src.schemas.models import Jobs, Skills, Experiences, Educations, Certificates
+from sqlalchemy.orm import Session
 from src.db_utils.db_connector import db_connect
 from src.feature_extraction.jobInfoExtraction import JobInfoExtraction
 from src.feature_extraction.Matcher import Matcher
 from resources import DEGREE_IMPORTANCE
 import json
-import pandas as pd
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 
 
-def transform_dict_to_json(data_dict):
-    json_data = json.dumps(data_dict, indent=4)
-    return json_data
+def get_db():
+    db = db_connect("postgresql", "username", "host", "database") 
+    try:
+        yield db
+    finally:
+        db.close()
 
-def add_to_database(job_entities, session, model, entity):
+def add_to_database(job_entities, model, entity, session= Depends(get_db)):
     for label in job_entities[entity]:
         found = session.query(model).filter(model.name == label).first()
 
@@ -22,52 +25,43 @@ def add_to_database(job_entities, session, model, entity):
     session.commit()
     session.close()
 
-app = FastAPI()
-
-
-@app.get("/extraction/{job_id}")
-def extract_job(job_id):
+# Define a dependency for job extraction
+def get_job_extractor(db: Session = Depends(get_db)):
     skills_pattern_path = "resources/data/skills.jsonl"
     model_path = "model_path"
     tokenizer_path = "tokenizer_path"
+    return JobInfoExtraction(model_path, tokenizer_path, DEGREE_IMPORTANCE, skills_pattern_path, db, Jobs)
 
-    # Connect to database:
-    session = db_connect("postgresql", "username", "host", "database")
-    
-    # Initialize the job extractor
-    job_extractor = JobInfoExtraction(model_path, tokenizer_path, DEGREE_IMPORTANCE, skills_pattern_path, session, Jobs)
 
+app = FastAPI()
+
+
+# Define the endpoint
+@app.get("/extraction/{job_id}")
+async def extract_job(job_id, job_extractor: JobInfoExtraction = Depends(get_job_extractor)):
     # Extract job information
     job_entities = job_extractor.extract_entities(job_id)
 
-    # Add Extracted skills to the database:
-    add_to_database(job_entities, session, Skills, "skills")
+    if job_entities is None:
+        raise HTTPException(status_code=404, detail="Job not found")
 
-    # Add Extracted experiences to the database:
-    add_to_database(job_entities, session, Experiences, "experiences")
-
-    # Add Extracted educations to the database:
-    add_to_database(job_entities, session, Educations, "degrees")
-
-    # Add Extracted certificates to the database:
-    add_to_database(job_entities, session, Certificates, "certificates")
-
-    # Add Extracted Job title to the database:
-    add_to_database(job_entities, session, Jobs, "job_title")
+    return job_entities
 
 
-    # Convert the job data to JSON
-    job_json = transform_dict_to_json(job_entities)
+@app.get("/matching/{job_id}")
+async def matching(job_id):
 
+    # Connect to database:
+    session = db_connect("postgresql", "username", "host", "database")
 
-    return job_json
-
-
+    # Get the job offer:
+    job = session.query(Jobs).filter(Jobs.id == job_id).first()
 
 
 
 
 if __name__ == "__main__":
-    pass
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
